267e5ff43ea4146d6140873c2830b22fa45c5b50
Kehinde Ogunyale <kehinde@chatdesk.com>
2019-11-10 22:52:42 +0100

    Binary Search Algorithm

---
+++ BinarySearch.py
@@ -0,0 +1,19 @@
+def BinarySearch(arr, k):
+    first = 0
+    end = len(arr) - 1
+    arr.sort()
+    found = False
+
+    while first <= end and not found:
+        mid = (first + end) // 2
+
+        if arr[mid] == k:
+            found = True
+            return found
+        else:
+            if k < arr[mid]:
+                end = mid -1
+            else:
+                first = mid + 1
+
+    return found
